5a3f6948c58fa8711a5d5b513093c945ecb44acf
Saepi <92858491+Saepi@users.noreply.github.com>
2023-12-14 14:09:04 +0300

    Update Suffix_array.py

--- Suffix_array.py
+++ Suffix_array.py
@@ -10,47 +10,50 @@ class SuffixArray:
         self.suffix_array = self.build_suffix_array()
 
     def build_suffix_array(self) -> list:
-
         suffixes = [i for i in range(len(self.text))]
         suffixes.sort(key=lambda i: self.text[i:])
 
         return suffixes
 
     def search_pattern(self, pattern: str) -> list:
-
         low, high = 0, len(self.text)
         loci = []
+        
         while low < high:
             mid = (low + high) // 2
             suffix = self.suffix_array[mid]
+            
             if self.text[suffix:suffix + len(pattern)] == pattern:
-                loci.append(suffix + 1)  # Adding 1 to make the output 1-indexed
-                # Continue searching for other occurrences
+                loci.append(suffix + 1)  # Добавляем единицу, чтобы вывод начинался с одного (а не с нуля)
                 left, right = mid - 1, mid + 1
+                
                 while left >= low and self.text[self.suffix_array[left]:self.suffix_array[left] + len(pattern)] == pattern:
                     loci.append(self.suffix_array[left] + 1)
                     left -= 1
+                    
                 while right < high and self.text[self.suffix_array[right]:self.suffix_array[right] + len(pattern)] == pattern:
                     loci.append(self.suffix_array[right] + 1)
                     right += 1
+                    
                 return loci
             elif self.text[suffix:suffix + len(pattern)] < pattern:
                 low = mid + 1
             else:
                 high = mid
-
-
+                
         return loci
 
 def align_sequences(pattern: str, text: str):
     suffix_array = SuffixArray(text)
     loci = suffix_array.search_pattern(pattern)
+    
     return loci
 
 def read_fasta(file_path: Union[str, Path]) -> str:
     with open(file_path, 'r') as file:
         lines = file.readlines()
     sequence = ''.join(line.strip() for line in lines[1:])
+    
     return sequence
 
 def read_output(output_path: Union[str, Path]) -> str:
@@ -67,9 +70,7 @@ def test(test_path: Union[str, Path]) -> None:
         pattern_file_path = test / "P.fa"
         text_file_path = test / "T.fa"
         output_file = test / "output.txt"
-
         
-
         pattern = read_fasta(pattern_file_path)
         text = read_fasta(text_file_path)
         alignment_loci = align_sequences(pattern, text)
@@ -98,7 +99,6 @@ def test(test_path: Union[str, Path]) -> None:
         print('')
 
 
-    
 
 if __name__ == "__main__":
 
